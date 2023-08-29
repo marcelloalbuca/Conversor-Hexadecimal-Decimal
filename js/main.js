706847b64f7b76276ed0325cffe571aab683ef58
Marcello Cavalcanti de Albuquerque <marcello.albuquerque@larcopetroleo.com.br>
2023-08-29 08:54:31 -0300

    Alert, regex

--- js/main.js
+++ js/main.js
@@ -1,42 +1,84 @@
+$(function() {
 
-function nCartão(){
+  $(".cep").hide();
+  $(".alert").hide();
 
-    var a,b;
-    var re = 0;
-    var r ='';
+  var input = $('input');
+  input.on("paste", function (evt) {
 
-    var a1 = document.getElementById("a1").value;
-    var a2 = document.getElementById("a2").value;
-    var a3 = document.getElementById("a3").value;
-    var a4 = document.getElementById("a4").value;
+  var x = input.val(input.val());
 
-    var valor2 = a4 + a3 + a2 + a1;
+  navigator.clipboard.writeText(x);
 
-    var valor2 = valor2.toUpperCase();
+  $('input').on("paste", function (evt) { 
+    setTimeout(function () {
+        var dados = $('input').val();
 
-    for(var i=0;i<valor2.length;i++){
-        a=valor2.charAt(i);
-        b=(a=='A')?10:((a=='B')?11:((a=='C')?12:((a=='D')?13:((a=='E')?14:((a=='F')?15:a)))));
-    
-        ant=b*Math.pow(16,(valor2.length-i-1));
-        r+=+valor2.charAt(i)+' x 16<sup>'+(valor2.length-i-1)+ant;
-        re+=ant;
-      }
+        var arr = dados.split('');
 
-    r+=re;
+        var a1 = (arr[0] + arr[1]);
 
-    var resultado = String(re);
+        var a2 = (arr[3] + arr[4]);
+
+        var a3 = (arr[6] + arr[7]);
+
+        var a4 = (arr[9] + arr[10]);
+
+        var a,b;
+        var re = 0;
+        var r ='';
+
+        var valor2 = a4 + a3 + a2 + a1;
+
+        var valor2 = valor2.toUpperCase();
+
+        let regex = /[^A-Za-z0-9 {8,8}]+/;
+
+        var validacao = regex.test(valor2);
+
+        //console.log(regex.test(valor2));
+
+        if (validacao == true){
+          //console.log("Contêm caracteres errados.")
+          $(".alert").show();
+        }else{
+          $(".alert").hide();
+          //console.log("Não contêm caracteres errados.");
+        
+
+        for(var i=0;i<valor2.length;i++){
+          a=valor2.charAt(i);
+          b=(a=='A')?10:((a=='B')?11:((a=='C')?12:((a=='D')?13:((a=='E')?14:((a=='F')?15:a)))));
+      
+          ant=b*Math.pow(16,(valor2.length-i-1));
+          r+=+valor2.charAt(i)+' x 16<sup>'+(valor2.length-i-1)+ant;
+          re+=ant;
+        }
   
-    if(resultado.length <= 9){
-      resultado = "0" + resultado;
-    }
+          r+=re;
+  
+          var resultado = String(re);
     
-    document.getElementById("n").innerHTML = "" + resultado;
-    $(".cep").show(" 0" + resultado);
-    $("#ncartao").html("Numeração do Cartão:" + "    " + resultado);
+        if(resultado.length <= 9){
+          resultado = "0" + resultado;
+        }
+      
+      document.getElementById("n").innerHTML = "" + resultado;
+      $(".cep").show(" 0" + resultado);
+      $("#ncartao").html("Numeração do Cartão:" + "    " + resultado);
 
-}
+      const resultado2 = resultado;
 
-$(function(){
-    $(".cep").hide();
+      navigator.clipboard.writeText(resultado2);
+  
+      return resultado;
+
+    }
+
+      },100);
+  
+    });
+
+  })
+
 });
